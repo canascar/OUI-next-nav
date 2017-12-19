a01b91f89c981687f320e2e1c946f77b72df7822
CJ Cenizal <cj@cenizal.com>
2017-12-19 08:07:19 -0800

    Fix typo in 0.0.7 changelog (#218)
    * Updated documentation.
    * 0.0.7
    * Fix typo in CHANGELOG.
    * Add wiki links to README.

--- docs/bundle.js
+++ docs/bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66b84f7cd59792df0ade86cc0f96bdc60f6b8fc8d7d5aa6fc8b643b880517582
-size 7017709
+oid sha256:490aec5155489d7fbe18430881ad75c8eb00d049d97a7e6046b09bdd6d2de2bf
+size 7033627
