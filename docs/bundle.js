19e70592f2b4f3a05ce71ca9f65a88938e265f7e
spalger <spalger@users.noreply.github.com>
2018-01-12 14:50:33 -0700

    Updated documentation.

--- docs/bundle.js
+++ docs/bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fadebbca8225d7c34a723f61986fb6838fccb7fac8b5b0da031c13557cc41e53
-size 7223179
+oid sha256:4e24201cb34ad5fce6a9abb95ad9c39b0d45110a721f74b72a8f56e665e5f9d9
+size 7199628
