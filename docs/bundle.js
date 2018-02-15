88e41b2f406c97123b6a20145ba1f05c1aebf135
Rory Hunter <roryhunter2@gmail.com>
2018-02-15 17:00:23 +0000

    Updated documentation.

--- docs/bundle.js
+++ docs/bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b33b8da2a5dcb97e38c8118d3a3093d1318d46237d1db742c289bd23e8a6d087
-size 8855539
+oid sha256:dc2d0dcd8d41049372140d57fbd85d493ee2e45fc0b51f69cd277260e13bb970
+size 9304722
