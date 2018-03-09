6e9df346f9cc5c85871e9d7e80b8bd4c1c361377
CJ Cenizal <cj@cenizal.com>
2018-03-09 12:25:49 -0800

    Update docs for 0.0.25.

--- docs/bundle.js
+++ docs/bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:248803b123b448361a007f5e27a925fa716115d0b7d8128329ddceb4d3fc5c69
-size 9524001
+oid sha256:6d9c9ff3e4532ee872211855fdde2f6a53198a9b2fa3c35916e5366bda1ac189
+size 9523984
