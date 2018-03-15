7503eecf9156bd03e9ab9a05c6924a877068f5dc
Nathan Reese <reese.nathan@gmail.com>
2018-03-15 14:07:23 -0600

    Updated documentation.

--- docs/bundle.js
+++ docs/bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:179cf6479d701380a639583d1cae36f2171fe3cc2582f141a6dc33ac18e98a58
-size 9466984
+oid sha256:928ddeebca88b1529d5bafd577f982a2a5465750eb674addf989d296edb58243
+size 9497195
