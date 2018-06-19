44fd8d03ac1a28d913e5205976154605147bfab4
Chandler Prall <chandler.prall@gmail.com>
2018-06-18 21:51:42 -0600

    Updated documentation.

--- docs/bundle.js
+++ docs/bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1c17edd35bd9e13419d639d1403beebf0eacf60e9ba8b04d5e49072260c4b1ea
-size 11959590
+oid sha256:b6396390ad94c4dbdd37c9bf24eb7867db5b47fa3e5975e49e4baadef476749a
+size 11960625
