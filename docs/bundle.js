9817157bf9705028881e979916287a5d9ec03018
Nicolás Bevacqua <nicolasbevacqua@gmail.com>
2018-09-10 19:45:02 -0300

    Updated documentation.

--- docs/bundle.js
+++ docs/bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:17e930d3e6aeeb895977c6c6d68905d9036df2abbb45dd56017ed754fc6a58a0
-size 14594485
+oid sha256:cc8b0ef80f42e0cd061bf60b02fefe16bde1e220dc33f81dca7fe081cd5d7a26
+size 14598317
