53ae9b9e43ed156bdb34a764e7aa27fa458cc7ba
Dave Snider <dave.snider@gmail.com>
2018-10-31 13:40:51 -0700

    Updated documentation.

--- docs/bundle.js
+++ docs/bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44aace08e5df6f558c7a571f3ff75224c5971f21e8e8ce12a1b14612acd58cfc
-size 14800418
+oid sha256:b51efdca09d2fd6e1b53c65cc98806abe7a07bb72fb1ff14761dde5c22a05685
+size 14920998
