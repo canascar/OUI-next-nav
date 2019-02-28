185a82b6d2a4704cb083a7529f6d8647547e6299
Greg Thompson <thompson.glowe@gmail.com>
2019-02-28 17:11:25 -0600

    Updated documentation.

--- docs/bundle.js
+++ docs/bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0e4bf00f2a8640ac41f702e9e056bd61d6df0a419cac2c3adcf306d55125c0f
-size 18116596
+oid sha256:6ff1f94b103cb90db9b40b81c4b9a6acb29d3657a61c867d531b9618cd6dd89a
+size 18118101
