b909d9e95590fba1a263924584491b19c530f6e2
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2019-03-21 14:30:19 -0400

    Updated documentation.

--- docs/bundle.js
+++ docs/bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e6c2a77850add2a0881ae9d13d9873e0ac7a07173fcf89a962a59f5fe270771
-size 18289399
+oid sha256:d9c770aa39726fed4f346f54ff746fff670ba00a5fe76854e02b28b4d555fe3e
+size 18271317
