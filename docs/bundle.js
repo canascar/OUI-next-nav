fa25c5a268c3581b7b29612d482230e3db04ab94
Carlos Canas <canascar@amazon.com>
2026-06-04 13:23:36 -0700

    Fix GitHub Pages: relative chunk paths + .nojekyll

--- docs/bundle.js
+++ docs/bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5db7f45537dffde262d6abe0326c9fccf1c63f35ef4701c365d1ef93cc0d0230
-size 59174066
+oid sha256:842e4fe3712001eb7dcfdf9c121745ca533e6a5baa15b0e020e218f3bd63732a
+size 59174067
