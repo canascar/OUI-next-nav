4f3bde6cedd26c06bf13144b97b2b02b25a740fc
Carlos Canas <canascar@amazon.com>
2026-06-08 11:49:07 -0700

    Sync further-glass changes

--- src-docs/src/views/sample_pages/sample_pages_left_nav.js
+++ src-docs/src/views/sample_pages/sample_pages_left_nav.js
@@ -1757,20 +1757,20 @@ const ProfilePopoverContent = () => {
           <span>Roles and identities</span>
         </button>
         <div className="samplePagesLeftNav__toolsPopoverGroup">
-          <div className="samplePagesLeftNav__toolsPopoverItem samplePagesLeftNav__toolsPopoverItem--parent">
+          <div
+            className="samplePagesLeftNav__toolsPopoverItem samplePagesLeftNav__toolsPopoverItem--parent"
+            style={{ cursor: 'pointer' }}
+            onClick={() => setHelpOpen(!helpOpen)}>
             <div className="samplePagesLeftNav__navItemIconWrap">
               <OuiIcon type="help" size="m" />
             </div>
             <span className="samplePagesLeftNav__toolsPopoverItemLabel">
               Help
             </span>
-            <OuiButtonIcon
-              iconType={helpOpen ? 'minus' : 'plus'}
-              aria-label="Toggle Help"
-              size="xs"
-              color="text"
-              display="empty"
-              onClick={() => setHelpOpen(!helpOpen)}
+            <OuiIcon
+              type={helpOpen ? 'minus' : 'plus'}
+              size="s"
+              color="subdued"
             />
           </div>
           {helpOpen && (
@@ -1778,7 +1778,7 @@ const ProfilePopoverContent = () => {
               <button
                 type="button"
                 className="samplePagesLeftNav__toolsPopoverItem samplePagesLeftNav__toolsPopoverItem--child"
-                onClick={() => {}}>
+                onClick={() => { window.location.hash = '#/'; }}>
                 <div className="samplePagesLeftNav__treeLine" />
                 <span>Documentation</span>
               </button>
@@ -3836,7 +3836,7 @@ export const SamplePagesLeftNav = ({
               </div>
               {showUserHelp && (
                 <div className="userMenuChildren">
-                  <div className="userMenuChild">Documentation</div>
+                  <div className="userMenuChild" onClick={() => { window.location.hash = '#/'; }}>Documentation</div>
                   <div className="userMenuChild">Community</div>
                   <div className="userMenuChild">Give feedback</div>
                   <div className="userMenuChild">Keyboard shortcut</div>
