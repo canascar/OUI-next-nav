1025740eefc86f6366b2c0a4f227b8011ea73633
Hoang Nguyen <jasonlh@amazon.com>
2026-06-24 23:41:39 -0700

    Remove shimmer demo page and all references

--- docs/bundle.js
+++ docs/bundle.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:842e4fe3712001eb7dcfdf9c121745ca533e6a5baa15b0e020e218f3bd63732a
-size 59174067
+oid sha256:6fc6e99daa95e028650d3288cd670c982461003307cca0b02ea8a728e2f3e75b
+size 153034228
